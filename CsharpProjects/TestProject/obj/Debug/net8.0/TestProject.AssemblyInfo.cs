43d9ee929a549f0bd44f91806451d1350221ed9d
Christopher Cortes <christopherctes@hotmail.com>
2024-11-15 19:19:54 -0300

    Folder replaced to root

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3d8b54fefe4ef7fc412620dcd687cc1e1bbf0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e17b8fd6c51873d0ececbd6337fc9ef5d6fe591")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
